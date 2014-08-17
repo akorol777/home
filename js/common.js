c5761fd1a9348a110be859abcb9befbd0e6d6a78
akorol777 <akorol777@ya.ru>
2014-08-17 03:11:12 +0300

    exspanel done

--- js/common.js
+++ js/common.js
@@ -1,19 +1,13 @@
 $(document).ready(function() {
-	// $(".btn").click(function(){
-	// 		//console.log('test');
-	// 		var selected = $(this).attr('href');	
-	// 		$.scrollTo(selected, 500);		
-	// 		return false;
-	// 	});
- 		$("a.btn").click(function() {
-        $("html, body").animate({
-            scrollTop: $($(this).attr("href")).offset().top + "px"
-        }, {
-            duration: 500
-        });
-        return false;
+ 		
+	$("a.btn").click(function() {
+    $("html, body").animate({
+      scrollTop: $($(this).attr("href")).offset().top + "px"
+    }, {
+      duration: 500
     });
-
+    return false;
+	});
 
 });
 
