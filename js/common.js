ad4ae1f6eaa9c716e4e8917666586a100281cb06
akorol777 <akorol777@ya.ru>
2014-08-17 14:58:39 +0300

    reload

--- js/common.js
+++ js/common.js
@@ -9,6 +9,99 @@ $(document).ready(function() {
     return false;
 	});
 
+
+
+	$( ".js-open-timelinecontent1" ).hover(
+	  function() {
+	    $( ".timelinecontent_1" ).addClass("is-show");
+	  }, function() {
+	    $( ".timelinecontent_1" ).removeClass("is-show");
+	  }
+	);
+	$( ".js-open-timelinecontent2" ).hover(
+	  function() {
+	    $( ".timelinecontent_2" ).addClass("is-show");
+	  }, function() {
+	    $( ".timelinecontent_2" ).removeClass("is-show");
+	  }
+	);
+	$( ".js-open-timelinecontent3" ).hover(
+	  function() {
+	    $( ".timelinecontent_3" ).addClass("is-show");
+	  }, function() {
+	    $( ".timelinecontent_3" ).removeClass("is-show");
+	  }
+	);
+	$( ".js-open-timelinecontent4" ).hover(
+	  function() {
+	    $( ".timelinecontent_4" ).addClass("is-show");
+	  }, function() {
+	    $( ".timelinecontent_4" ).removeClass("is-show");
+	  }
+	);
+	$( ".js-open-timelinecontent5" ).hover(
+	  function() {
+	    $( ".timelinecontent_5" ).addClass("is-show");
+	  }, function() {
+	    $( ".timelinecontent_5" ).removeClass("is-show");
+	  }
+	);
+	$( ".js-open-timelinecontent6" ).hover(
+	  function() {
+	    $( ".timelinecontent_6" ).addClass("is-show");
+	  }, function() {
+	    $( ".timelinecontent_6" ).removeClass("is-show");
+	  }
+	);
+	$( ".js-open-timelinecontent7" ).hover(
+	  function() {
+	    $( ".timelinecontent_7" ).addClass("is-show");
+	  }, function() {
+	    $( ".timelinecontent_7" ).removeClass("is-show");
+	  }
+	);
+	$( ".js-open-timelinecontent8" ).hover(
+	  function() {
+	    $( ".timelinecontent_8" ).addClass("is-show");
+	  }, function() {
+	    $( ".timelinecontent_8" ).removeClass("is-show");
+	  }
+	);
+	$( ".js-open-timelinecontent9" ).hover(
+	  function() {
+	    $( ".timelinecontent_9" ).addClass("is-show");
+	  }, function() {
+	    $( ".timelinecontent_9" ).removeClass("is-show");
+	  }
+	);
+	$( ".js-open-timelinecontent10" ).hover(
+	  function() {
+	    $( ".timelinecontent_10" ).addClass("is-show");
+	  }, function() {
+	    $( ".timelinecontent_10" ).removeClass("is-show");
+	  }
+	);
+	$( ".js-open-timelinecontent11" ).hover(
+	  function() {
+	    $( ".timelinecontent_11" ).addClass("is-show");
+	  }, function() {
+	    $( ".timelinecontent_11" ).removeClass("is-show");
+	  }
+	);
+	$( ".js-open-timelinecontent12" ).hover(
+	  function() {
+	    $( ".timelinecontent_12" ).addClass("is-show");
+	  }, function() {
+	    $( ".timelinecontent_12" ).removeClass("is-show");
+	  }
+	);
+	$( ".js-open-timelinecontent13" ).hover(
+	  function() {
+	    $( ".timelinecontent_13" ).addClass("is-show");
+	  }, function() {
+	    $( ".timelinecontent_13" ).removeClass("is-show");
+	  }
+	);
 });
 
 
